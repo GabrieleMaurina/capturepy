ccc2b87a2f28ec94419905f80cbc04c95d5f0bd9
GabrieleMaurina <gabrielemaurina95@gmail.com>
2020-12-10 16:38:45 +0100

    Fixed missing comman.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
 	version=output_capture.__version__,
 	author='Gabriele Maurina',
 	author_email='gabrielemaurina95@gmail.com',
-	description='output_capture captures the standard output and the standard error of functions, methods and any blocks of code. It is useful when testing, debugging and working with libraries. Find out more at https://github.com/GabrieleMaurina/output_capture.'
+	description='output_capture captures the standard output and the standard error of functions, methods and any blocks of code. It is useful when testing, debugging and working with libraries. Find out more at https://github.com/GabrieleMaurina/output_capture.',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
 	url='https://github.com/GabrieleMaurina/capture_output',
